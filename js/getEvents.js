7164bfee03e72bac4c75bb9e9847751ac52c9d8a
makio135 <lionel.radisson@gmail.com>
2018-03-16 11:22:14 +0100

    better cards layout

--- js/getEvents.js
+++ js/getEvents.js
@@ -1,22 +1,24 @@
 addEventListener( 'load', e => {
     const formatEvent = event => {
         return `
-            <div class="col s12 m6 card">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src="https://placeimg.com/640/480/tech">
-                </div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="https://placeimg.com/640/480/tech">
+                    </div>
 
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">${ event[ 0 ] }<i class="material-icons right">more_vert</i></span>
-                    <p>
-                        WHERE: makers' lab ${ event[ 1 ] }<br>
-                        WHEN:${ event[ 2 ] } -- ${ event[ 3 ] }
-                    </p>
-                </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">${ event[ 0 ] }<i class="material-icons right">more_vert</i></span>
+                        <p>
+                            WHERE: <span class="red-text">makers' lab ${ event[ 1 ] }</span><br>
+                            WHEN: <span class="red-text">${ event[ 2 ] } -- ${ event[ 3 ] }</span>
+                        </p>
+                    </div>
 
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">${ event[ 0 ] }<i class="material-icons right">close</i></span>
-                    <p>${ event[ 4 ] }</p>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">${ event[ 0 ] }<i class="material-icons right">close</i></span>
+                        <p>${ event[ 4 ] }</p>
+                    </div>
                 </div>
             </div>
         `;
