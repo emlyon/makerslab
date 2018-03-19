45056db60062f3441057fe0dbbf798196b2e82b8
MAKIO135 <makio135@gmail.com>
2018-03-19 14:48:06 +0100

    styleguide

--- js/agenda.js
+++ js/agenda.js
@@ -60,11 +60,11 @@ addEventListener( 'load', e => {
 
     const slugify = text =>
         text.toString().toLowerCase()
-            .replace(/\s+/g, '-')           // Replace spaces with -
-            .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-            .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-            .replace(/^-+/, '')             // Trim - from start of text
-            .replace(/-+$/, '');            // Trim - from end of text
+            .replace( /\s+/g, '-' )           // Replace spaces with -
+            .replace( /[^\w\-]+/g, '' )       // Remove all non-word chars
+            .replace( /\-\-+/g, '-' )         // Replace multiple - with single -
+            .replace( /^-+/, '' )             // Trim - from start of text
+            .replace( /-+$/, '' );            // Trim - from end of text
 
     const formSubmission = () => {
         [].forEach.call( document.querySelectorAll( 'form' ), form => {
