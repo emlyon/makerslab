3458393d0d154cb0984c04cd1eccf1337f6a6906
MAKIO135 <makio135@gmail.com>
2018-06-05 10:20:18 +0200

    add icons

--- includes/head.js
+++ includes/head.js
@@ -1,12 +1,19 @@
 include( `
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>makers' lab</title>
 
-    <!-- CSS  -->
+    <!-- CSS -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css" rel="stylesheet" media="screen,projection" />
-    <link href="css/style.min.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css" rel="stylesheet" media="screen,projection">
+    <link href="css/style.min.css" type="text/css" rel="stylesheet" media="screen,projection">
     <!-- Material ScrollTop stylesheet -->
     <link rel="stylesheet" href="css/material-scrolltop.css">
+
+    <!-- Icons -->
+    <link rel="icon" href="https://emlyon.github.io/makerslab/icons/cropped-earlymakers-32x32.jpg" sizes="32x32">
+    <link rel="icon" href="https://emlyon.github.io/makerslab/icons/cropped-earlymakers-192x192.jpg" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="https://emlyon.github.io/makerslab/icons/cropped-earlymakers-180x180.jpg">
+    <meta name="msapplication-TileImage" content="https://emlyon.github.io/makerslab/icons/cropped-earlymakers-270x270.jpg">
+    <link rel="shortcut icon" href="https://emlyon.github.io/makerslab/icons/cropped-earlymakers-270x270.jpg">
 ` );
