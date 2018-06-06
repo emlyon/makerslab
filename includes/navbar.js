3661d892b5fd79f2ac2757bc03614faa9f3b54bd
MAKIO135 <makio135@gmail.com>
2018-06-06 16:19:26 +0200

    use includes + learn page

--- includes/navbar.js
+++ includes/navbar.js
@@ -5,21 +5,15 @@ include( `
                 <a id="logo-container" href="." class="brand-logo"><img src="media/logo.png" alt="makers' lab"/></a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href=".">makers' lab</a></li>
-                    <li><a href="toolbox.html">how to</a></li>
-                    <li><a href="workshops.html">workshops</a></li>
-                    <li><a href="contents.html">contents</a></li>
-                    <!-- <li><a href="boosters.html">boosters</a></li> -->
-                    <!-- <li><a href="events.html">events</a></li> -->
-                    <!-- <li><a href="resources.html">resources</a></li> -->
+                    <li><a href="discover.html">discover</a></li>
+                    <li><a href="experiment.html">experiment</a></li>
+                    <li><a href="learn.html">learn</a></li>
                 </ul>
                 <ul id="nav-mobile" class="side-nav">
                     <li><a href=".">makers' lab</a></li>
-                    <li><a href="toolbox.html">how to</a></li>
-                    <li><a href="workshops.html">workshops</a></li>
-                    <li><a href="contents.html">contents</a></li>
-                    <!-- <li><a href="boosters.html">boosters</a></li> -->
-                    <!-- <li><a href="events.html">events</a></li> -->
-                    <!-- <li><a href="resources.html">resources</a></li> -->
+                    <li><a href="discover.html">discover</a></li>
+                    <li><a href="experiment.html">experiment</a></li>
+                    <li><a href="learn.html">learn</a></li>
                 </ul>
                 <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
             </div>
