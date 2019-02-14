afc918c3fec3063e055059fa524be786823c1aed
makio135 <radisson@MAC22219-6.local>
2019-02-14 22:08:26 +0100

    working on next version

--- includes/navbar.js
+++ includes/navbar.js
@@ -2,7 +2,7 @@ include( `
     <div class="navbar-fixed">
         <nav class="white" role="navigation">
             <div class="nav-wrapper container">
-                <a id="logo-container" href="." class="brand-logo"><img src="media/logo.png" alt="makers' lab"/></a>
+                <a id="logo-container" href="." class="brand-logo"><img src="media/logo.svg" alt="makers' lab"/></a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href=".">makers' lab</a></li>
                     <li><a href="skills.html">skills</a></li>
