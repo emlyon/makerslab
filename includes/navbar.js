c138f392a6311d6194408b8bc27a3bd4abce50b4
makio135 <radisson@MAC22219-6.local>
2019-02-15 15:58:17 +0100

    emphasize current page in navbar

--- includes/navbar.js
+++ includes/navbar.js
@@ -2,18 +2,18 @@ include( `
     <div class="navbar-fixed">
         <nav class="white" role="navigation">
             <div class="nav-wrapper container">
-                <a id="logo-container" href="." class="brand-logo"><img src="media/logo.svg" alt="makers' lab"/></a>
+                <a id="logo-container" href="." class="brand-logo"><img src="media/logo.png" alt="makers' lab"/></a>
                 <ul class="right hide-on-med-and-down">
-                    <li><a href=".">makers' lab</a></li>
-                    <li><a href="skills.html">skills</a></li>
-                    <li><a href="workshops.html">workshops</a></li>
-                    <li><a href="courses.html">courses</a></li>
+                    <li><a href="/">makers' lab</a></li>
+                    <li><a href="/skills.html">skills</a></li>
+                    <li><a href="/workshops.html">workshops</a></li>
+                    <li><a href="/courses.html">courses</a></li>
                 </ul>
                 <ul id="nav-mobile" class="side-nav">
-                    <li><a href=".">makers' lab</a></li>
-                    <li><a href="skills.html">skills</a></li>
-                    <li><a href="workshops.html">workshops</a></li>
-                    <li><a href="courses.html">courses</a></li>
+                    <li><a href="/">makers' lab</a></li>
+                    <li><a href="/skills.html">skills</a></li>
+                    <li><a href="/workshops.html">workshops</a></li>
+                    <li><a href="/courses.html">courses</a></li>
                 </ul>
                 <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
             </div>
