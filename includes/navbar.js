d9fc0c62f9ed2020fd83d12403fc6a7be93ac3f7
sjvl <samueljavelle@hotmail.com>
2020-03-26 12:01:14 +0100

    give access to lite courses

--- includes/navbar.js
+++ includes/navbar.js
@@ -5,13 +5,13 @@ include( `
                 <a id="logo-container" href="." class="brand-logo"><img src="media/logo.png" alt="makers' lab"/></a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href="/">makers' lab</a></li>
-                    <li><a href="/skills.html">skills</a></li>
+                    <li><a href="/skills.html">tutorials</a></li>
                     <li><a href="/workshops.html">workshops</a></li>
                     <li><a href="/courses.html">courses</a></li>
                 </ul>
                 <ul id="nav-mobile" class="side-nav">
                     <li><a href="/">makers' lab</a></li>
-                    <li><a href="/skills.html">skills</a></li>
+                    <li><a href="/skills.html">tutorials</a></li>
                     <li><a href="/workshops.html">workshops</a></li>
                     <li><a href="/courses.html">courses</a></li>
                 </ul>
