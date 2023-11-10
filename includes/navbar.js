86265d903cb144a8998b39f0b14d08ee6e476a99
Jetinho <jet.julien@gmail.com>
2023-11-10 09:39:28 +0100

    fix mobile navigation

--- includes/navbar.js
+++ includes/navbar.js
@@ -3,6 +3,7 @@ include(`
         <nav class="white" role="navigation">
             <div class="nav-wrapper container">
                 <a id="logo-container" href="/" class="brand-logo"><img src="/media/logofactory.svg" alt="makers' lab"/></a>
+                <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i style="color: #e2001a;" class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href="/">makers' lab</a></li>
                     <li><a href="/tutorials.html">tutorials</a></li>
@@ -12,17 +13,16 @@ include(`
                     <li class="lang-en"><a href="/">en</a></li>
                     <li class="lang-fr"><a href="/fr/">fr</a></li>
                 </ul>
-                <ul id="nav-mobile" class="sidenav">
-                    <li><a href="/">makers' lab</a></li>
-                    <li><a href="/tutorials.html">tutorials</a></li>
-                    <li><a href="/events.html">events</a></li>
-                    <li><a href="/courses.html">courses</a></li>
-                    <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
-                    <li class="lang-en"><a href="/">en</a></li>
-                    <li class="lang-fr"><a href="/fr/">fr</a></li>
-                </ul>
-                <a href="#" data-activates="nav-mobile" class="button-collapse"><i style="color: #e2001a;" class="material-icons">menu</i></a>
             </div>
         </nav>
     </div>
+    <ul id="nav-mobile" class="sidenav">
+        <li><a href="/">makers' lab</a></li>
+        <li><a href="/tutorials.html">tutorials</a></li>
+        <li><a href="/events.html">events</a></li>
+        <li><a href="/courses.html">courses</a></li>
+        <li class="divider"></li>
+        <li class="lang-en"><a href="/">en</a></li>
+        <li class="lang-fr"><a href="/fr/">fr</a></li>
+    </ul>
 `);
