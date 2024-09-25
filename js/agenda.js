0b210b8be2d466f205d6d3d1095b7f650a39888a
Jetinho <jet.julien@gmail.com>
2024-09-25 17:25:26 +0200

    fetch and display event data, with eventbrite popup widget

--- js/agenda.js
+++ js/agenda.js
@@ -4,160 +4,77 @@ const eventsList = document.getElementById('eventsList');
 // Fetch events.json file
 fetch('workshops-data/events.json')
   .then((response) => response.json())
-  .then((eventIds) => {
-    eventIds.forEach((eventId) => {
-      const li = document.createElement('li');
-      const div = document.createElement('div');
-      div.id = `eventbrite-widget-container-${eventId}`;
-      div.innerText = `Event ${eventId}`;
-      const buttonHtml = `<button id="triggerWidget${eventId}" type="button">
-        Register
-      </button>;`;
-      div.innerHTML = buttonHtml;
-      li.appendChild(div);
-      eventsList.appendChild(li);
+  .then((events) => {
+    let eventRow;
+    events.forEach((event, index) => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      const cardImage = document.createElement('div');
+      cardImage.classList.add('card-image');
+      cardImage.innerHTML = `<img style="object-fit:cover;" src="${event.logo?.url}">`;
 
-      var exampleCallback = function () {
-        console.log('Order complete!');
+      card.appendChild(cardImage);
+
+      const cardContent = document.createElement('div');
+      cardContent.classList.add('card-content');
+      cardContent.innerHTML = `<span class="card-title grey-text text-darken-4">${event.name.text}</span>`;
+      cardContent.innerHTML += `<div class="divider"></div>`;
+      cardContent.innerHTML += `<p>${event.description.text}</p>`;
+      // We could use ${event.venue.name} instead of 'makers' lab emlyon', but the city is also mentioned in the name, so we avoid redundancy.
+      cardContent.innerHTML += `<span class="red-text darken-4">📍</span> <strong class="red-text">${event.venue.address.city}</strong> - <strong>makers' lab emlyon</strong> <br>`;
+
+      // Display user friendly date and time
+      const startDate = new Date(event.start.local);
+      const endDate = new Date(event.end.local);
+      const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
       };
+      const formatStartDate = startDate.toLocaleDateString('en-UK', options);
+      const formatEndDate = endDate.toLocaleDateString('en-UK', options);
+      const startHour = startDate.toLocaleTimeString('en-UK', { hour: '2-digit', minute: '2-digit' });
+      const endHour = endDate.toLocaleTimeString('en-UK', { hour: '2-digit', minute: '2-digit' });
+      // If an event is on a single day, we display the date and the time separately.
+      if (formatStartDate === formatEndDate) {
+        const dateInfo = formatStartDate;
+        cardContent.innerHTML += `<p><span class="red-text darken-4">🗓️</span> ${dateInfo}</p>`;
+        const timeInfo = `${startHour} to ${endHour}`;
+        cardContent.innerHTML += `<p><span class="red-text darken-4">🕒</span> ${timeInfo}</p>`;
+      } else {
+        // If an event is on different days, we display the date and time for start and end date.
+        const dateAndTimeInfo = `${formatStartDate} - ${startHour} to ${formatEndDate} - ${endHour}`;
+        cardContent.innerHTML += `<span class="red-text darken-4">🗓️</span> ${dateAndTimeInfo}`;
+      }
+
+      const cta = document.createElement('div');
+      cta.style.marginTop = '15px';
+      cta.innerHTML = `<a class="waves-effect waves-light btn" id="triggerWidget${event.id}">Register</a>`;
+
+      cardContent.appendChild(cta);
+      card.appendChild(cardContent);
+
+      const eventCol = document.createElement('div');
+      eventCol.classList.add('col', 's12', 'm4');
+
+      if (index % 3 === 0) {
+        eventRow = document.createElement('div');
+        eventRow.classList.add('row');
+      }
+
+      eventCol.appendChild(card);
+      eventRow.appendChild(eventCol);
+      if (index % 3 === 2) {
+        eventsList.appendChild(eventRow);
+      }
 
       window.EBWidgets.createWidget({
         widgetType: 'checkout',
-        eventId: eventId,
+        eventId: event.id,
         modal: true,
-        modalTriggerElementId: `triggerWidget${eventId}`,
-        onOrderComplete: exampleCallback
+        modalTriggerElementId: `triggerWidget${event.id}`
       });
-
-      // window.EBWidgets.createWidget({
-      //   widgetType: 'checkout',
-      //   eventId: eventId,
-      //   iframeContainerId: `eventbrite-widget-container-${eventId}`,
-      //   iframeContainerHeight: 425
-      // });
     });
   })
   .catch((error) => console.error('Error fetching events:', error));
-
-// const parseEventData = (eventDataArray) => {
-//   let [title, campus, date, hour, desc, img, status, url] = eventDataArray;
-//   eventData = { title, campus, date, hour, desc, img, status };
-//   return { eventData };
-// };
-
-// const formatEvent = (event, i, eventBriteUrl) => {
-//   let { title, campus, date, hour, desc, img, status } = event;
-//   console.log({ title, campus, date, hour, desc, img, status });
-//   campus = campus.toUpperCase();
-//   status = status ? status.toUpperCase() : '';
-
-//   const soldout = status === 'SOLDOUT';
-//   const comingsoon = status === 'COMINGSOON';
-
-//   let cta;
-//   if (url) {
-//     cta = `<div style="margin-top:15px;">
-//             <a href="${url}" target="_blank" class="waves-effect waves-light btn activator">register</a>
-//         </div>`;
-//   } else {
-//     cta = `<div style="margin-top:15px;">
-//             <a class="waves-effect waves-light btn activator ${soldout || comingsoon ? 'disabled' : ''}">${
-//       soldout ? 'SOLD OUT' : comingsoon ? 'COMING SOON' : 'register'
-//     }</a>
-//         </div>`;
-//   }
-//   const includeForm = !eventBriteUrl;
-//   const htmlForm = `<form data-event="${title + '_' + campus + '_' + date}">
-//         <div class="row">
-//             <div class="input-field col l12">
-//                 <i class="material-icons prefix">account_circle</i>
-//                 <input id="name" type="text" class="validate">
-//                 <label for="name">Name</label>
-//             </div>
-
-//             <div class="input-field col l12">
-//                 <i class="material-icons prefix">phone</i>
-//                 <input id="phone" type="tel" class="validate">
-//                 <label for="phone">Telephone</label>
-//             </div>
-
-//             <div class="input-field col s12">
-//                 <i class="material-icons prefix">mail</i>
-//                 <input id="mail" type="email" class="validate">
-//                 <label for="mail" data-error="wrong" data-success="right">Email</label>
-//             </div>
-//         </div>
-
-//         <a class="waves-effect waves-light btn right register"><i class="material-icons left">send</i>register</a>
-
-//         <h5 class="on-success hide red-text">
-//             We have received your registration.<br>
-//             Thank you!
-//         </h5>
-
-//         <h5 class="on-error hide red-text">
-//             A problem happened during registration.<br>
-//             Please try again later!
-//         </h5>
-//     </form>`;
-
-//   return `
-//         ${i % 3 === 0 ? '<div class="row">' : ''}
-//         <div class="col s12 m4">
-//             <div class="card">
-//                 <div class="card-image waves-effect waves-block waves-light">
-//                     <img class="activator" style="object-fit:cover;" src="${img}">
-//                 </div>
-
-//                 <div class="card-content">
-//                     <span class="card-title activator grey-text text-darken-4">${title}${
-//     soldout || comingsoon ? '' : '<i class="material-icons right">more_vert</i>'
-//   }</span>
-//                     <div class="divider"></div>
-
-//                     <p>
-//                         <span class="red-text darken-4">ℹ️ :</span> ${desc}<br>
-//                         <span class="red-text darken-4">📍 :</span> makers' lab <b class="red-text">${
-//                           campus == 'ECU'
-//                             ? 'Écully'
-//                             : campus == 'PAR'
-//                             ? 'Paris'
-//                             : campus == 'STE'
-//                             ? 'Saint-Étienne'
-//                             : 'room Zoom'
-//                         }</b><br>
-//                         <span class="red-text darken-4">🗓️ :</span> ${date} -- ${hour}
-//                     </p>
-
-//                     ${cta}
-//                 </div>
-
-//                 ${
-//                   soldout || comingsoon
-//                     ? ''
-//                     : `<div class="card-reveal">
-//                     <span class="card-title grey-text text-darken-4">${title}<i class="material-icons right">close</i></span>
-//                     <div class="divider"></div>
-
-//                     <p>
-//                         <span class="red-text darken-4">WHERE:</span> makers' lab <b class="red-text">${
-//                           campus == 'ECU'
-//                             ? 'Écully'
-//                             : campus == 'PAR'
-//                             ? 'Paris'
-//                             : campus == 'STE'
-//                             ? 'Saint-Étienne'
-//                             : 'room Zoom'
-//                         }</b><br>
-//                         <span class="red-text darken-4">WHEN:</span> ${date} -- ${hour}
-//                     </p>
-//                     <div class="divider"></div>
-
-//                     ${includeForm ? htmlForm : ''}
-//                 </div>`
-//                 }
-//             </div>
-//         </div>
-//         ${i % 3 === 2 ? '</div>' : ''}
-//     `;
-// };
--- workshops-data/fetch.js
+++ workshops-data/fetch.js
@@ -1,80 +1,73 @@
 const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('workshops-data/eventbrite-config.json', 'utf8'));
+const TOKEN = config.TOKEN;
+const ORGANIZATION_ID = config.ORGANIZATION_ID;
+const OPTIONS = {
+  method: 'GET',
+  headers: {
+    Authorization: `Bearer ${TOKEN}`
+  }
+};
 
-// Use dynamic import for node-fetch
 (async () => {
+  // Fetch events and process them
+  try {
+    const events = await fetchEvents();
+    console.log("Nombre d'évènements : " + events.length);
+    await enrichEventsWithVenueData(events);
+    console.log('events with venues', events);
+
+    // Write formatted events to events.json
+    fs.writeFileSync('workshops-data/events.json', JSON.stringify(events, null, 2));
+    process.on('exit', () => {
+      console.log('Closing connection and exiting...');
+      // Perform any cleanup if necessary
+    });
+    process.exit();
+  } catch (e) {
+    console.warn(e);
+  }
+})();
+
+async function fetchEvents() {
   const fetch = (await import('node-fetch')).default;
-
-  // Read config.json file
-  const config = JSON.parse(fs.readFileSync('workshops-data/eventbrite-config.json', 'utf8'));
-
-  const ORGANIZATION_ID = config.ORGANIZATION_ID;
-  const TOKEN = config.TOKEN;
-
-  const baseUrl = `https://www.eventbriteapi.com/v3/organizations/${ORGANIZATION_ID}/events/?time_filter=current_future&status=live`;
-  // const baseUrl = `https://www.eventbriteapi.com/v3/organizations/${ORGANIZATION_ID}/events/`;
-  const options = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${TOKEN}`
-    }
-  };
-
-  // Il y a plein de paramètres disponibles pour filtrer les évènements : https://www.eventbrite.com/platform/api#/reference/event/list/list-events-by-organization
+  const baseUrl = `https://www.eventbriteapi.com/v3/organizations/${ORGANIZATION_ID}/events/`;
   const params = {
-    status: 'live'
+    status: 'live',
+    time_filter: 'current_future'
   };
 
   const url = new URL(baseUrl);
   url.search = new URLSearchParams(params).toString();
 
-  fetch(url, options)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const events = data.events;
-      console.log(events);
-      console.log("Nombre d'évènements : " + events.length);
-      // Clear existing events in events.json
-      // fs.writeFileSync('workshops-data/events.json', JSON.stringify([]));
+  try {
+    const response = await fetch(url, OPTIONS);
+    const data = await response.json();
+    return data.events;
+  } catch (e) {
+    console.warn(e);
+    throw e;
+  }
+}
 
-      // Map events to the desired format
-      // const formattedEvents = events.map(event => ({
-      //   title: event.name.text,
-      //   desc: event.description.text,
-      //   campus: event.venue.name,
-      //   date: new Date(event.start.utc).toLocaleDateString(),
-      //   hour: new Date(event.start.utc).toLocaleTimeString(),
-      //   img: event.logo ? event.logo.url : '',
-      //   status: event.status
-      // }));
+async function fetchVenues() {
+  const baseURL = `https://www.eventbriteapi.com/v3/organizations/${ORGANIZATION_ID}/venues/`;
+  const fetch = (await import('node-fetch')).default;
+  try {
+    const response = await fetch(baseURL, OPTIONS);
+    const data = await response.json();
+    return data.venues;
+  } catch (e) {
+    console.warn(e);
+    throw e;
+  }
+}
 
-      // Write formatted events to events.json
-      fs.writeFileSync(
-        'workshops-data/events.json',
-        JSON.stringify(
-          events.map((e) => e.id),
-          null,
-          2
-        )
-      );
-
-      // events.forEach((event, i) => {
-      // console.log(event);
-      // html += formatEvent(event.eventData, i, event.url);
-      // });
-      //   agenda.innerHTML = html;
-
-      // set height auto
-      // setTimeout(() => {
-      //   $('.agenda .card-content>p').height(
-      //     [].map.call($('.agenda .card-content>p'), (d) => $(d).height()).sort((a, b) => b - a)[0]
-      //   );
-      //   $('.agenda .card-image>img').css(
-      //     'min-height',
-      //     [].map.call($('.agenda .card-image>img'), (d) => $(d).height()).sort((a, b) => b - a)[0]
-      //   );
-      // }, 100);
-    })
-    .catch((e) => console.warn(e));
-})();
+async function enrichEventsWithVenueData(events) {
+  const venues = await fetchVenues();
+  events.forEach((event) => {
+    const venue = venues.find((v) => v.id === event.venue_id);
+    event.venue = venue;
+  });
+  return events;
+}
