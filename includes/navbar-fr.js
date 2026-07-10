e01698604a34673508a2e75d3694e212bafd48bb
Julien Jet <jet.julien@gmail.com>
2026-07-10 11:08:32 +0200

    fix fr navbar

--- includes/navbar-fr.js
+++ includes/navbar-fr.js
@@ -1,28 +1,30 @@
 include(`
-    <div class="navbar-fixed">
-        <nav class="white" role="navigation">
-            <div class="nav-wrapper container">
-                <a id="logo-container" href="/fr/" class="brand-logo"><img src="/media/logofactory.svg" alt="makers' lab"/></a>
-                <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i style="color: #e2001a;" class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="/fr/">makers' lab</a></li>
-                    <li><a href="/fr/tutoriels.html">tutoriels</a></li>
-                    <li><a href="/fr/evenements.html">événements</a></li>
-                    <li><a href="/fr/formations.html">formations</a></li>
-                    <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
-                    <li class="lang-en"><a href="/">en</a></li>
-                    <li class="lang-fr"><a href="/fr/">fr</a></li>
-                </ul>
-            </div>
-        </nav>
+    <div id="navbar">
+        <div class="navbar-fixed">
+            <nav class="white" role="navigation">
+                <div class="nav-wrapper container">
+                    <a id="logo-container" href="/fr/" class="brand-logo"><img src="/media/logofactory.svg" alt="makers' lab"/></a>
+                    <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i style="color: #e2001a;" class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="/fr/">makers' lab</a></li>
+                        <li><a href="/fr/tutoriels.html">tutoriels</a></li>
+                        <li><a href="/fr/evenements.html">événements</a></li>
+                        <li><a href="/fr/formations.html">formations</a></li>
+                        <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+                        <li class="lang-en"><a href="/">en</a></li>
+                        <li class="lang-fr"><a href="/fr/">fr</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <ul id="nav-mobile" class="sidenav">
+            <li><a href="/fr/">makers' lab</a></li>
+            <li><a href="/fr/tutoriels.html">tutoriels</a></li>
+            <li><a href="/fr/evenements.html">événements</a></li>
+            <li><a href="/fr/formations.html">formations</a></li>
+            <li class="divider"></li>
+            <li class="lang-en"><a href="/">en</a></li>
+            <li class="lang-fr"><a href="/fr/">fr</a></li>
+        </ul>
     </div>
-    <ul id="nav-mobile" class="sidenav">
-        <li><a href="/fr/">makers' lab</a></li>
-        <li><a href="/fr/tutoriels.html">tutoriels</a></li>
-        <li><a href="/fr/evenements.html">événements</a></li>
-        <li><a href="/fr/formations.html">formations</a></li>
-        <li class="divider"></li>
-        <li class="lang-en"><a href="/">en</a></li>
-        <li class="lang-fr"><a href="/fr/">fr</a></li>
-    </ul>
 `);
