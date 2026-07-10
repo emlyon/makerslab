e423c53f6aa38553b3f3102503d1e33937e4c5c6
Julien Jet <jet.julien@gmail.com>
2026-07-10 10:54:43 +0200

    display notice after popup is dismissed

--- includes/navbar.js
+++ includes/navbar.js
@@ -1,28 +1,30 @@
 include(`
-    <div class="navbar-fixed">
-        <nav class="white" role="navigation">
-            <div class="nav-wrapper container">
-                <a id="logo-container" href="${appPath('/')}" class="brand-logo"><img src="${appPath('/media/logofactory.svg')}" alt="makers' lab"/></a>
-                <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i style="color: #e2001a;" class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="${appPath('/')}">makers' lab</a></li>
-                    <li><a href="${appPath('/tutorials.html')}">tutorials</a></li>
-                    <li><a href="${appPath('/events.html')}">events</a></li>
-                    <li><a href="${appPath('/courses.html')}">courses</a></li>
-                    <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
-                    <li class="lang-en"><a href="${typeof languagePath === 'function' ? languagePath('en') : appPath('/')}">en</a></li>
-                    <li class="lang-fr"><a href="${typeof languagePath === 'function' ? languagePath('fr') : appPath('/fr/')}">fr</a></li>
-                </ul>
-            </div>
-        </nav>
+    <div id="navbar">
+        <div class="navbar-fixed">
+            <nav class="white" role="navigation">
+                <div class="nav-wrapper container">
+                    <a id="logo-container" href="${appPath('/')}" class="brand-logo"><img src="${appPath('/media/logofactory.svg')}" alt="makers' lab"/></a>
+                    <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i style="color: #e2001a;" class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="${appPath('/')}">makers' lab</a></li>
+                        <li><a href="${appPath('/tutorials.html')}">tutorials</a></li>
+                        <li><a href="${appPath('/events.html')}">events</a></li>
+                        <li><a href="${appPath('/courses.html')}">courses</a></li>
+                        <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+                        <li class="lang-en"><a href="${typeof languagePath === 'function' ? languagePath('en') : appPath('/')}">en</a></li>
+                        <li class="lang-fr"><a href="${typeof languagePath === 'function' ? languagePath('fr') : appPath('/fr/')}">fr</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <ul id="nav-mobile" class="sidenav">
+            <li><a href="${appPath('/')}">makers' lab</a></li>
+            <li><a href="${appPath('/tutorials.html')}">tutorials</a></li>
+            <li><a href="${appPath('/events.html')}">events</a></li>
+            <li><a href="${appPath('/courses.html')}">courses</a></li>
+            <li class="divider"></li>
+            <li class="lang-en"><a href="${typeof languagePath === 'function' ? languagePath('en') : appPath('/')}">en</a></li>
+            <li class="lang-fr"><a href="${typeof languagePath === 'function' ? languagePath('fr') : appPath('/fr/')}">fr</a></li>
+        </ul>
     </div>
-    <ul id="nav-mobile" class="sidenav">
-        <li><a href="${appPath('/')}">makers' lab</a></li>
-        <li><a href="${appPath('/tutorials.html')}">tutorials</a></li>
-        <li><a href="${appPath('/events.html')}">events</a></li>
-        <li><a href="${appPath('/courses.html')}">courses</a></li>
-        <li class="divider"></li>
-        <li class="lang-en"><a href="${typeof languagePath === 'function' ? languagePath('en') : appPath('/')}">en</a></li>
-        <li class="lang-fr"><a href="${typeof languagePath === 'function' ? languagePath('fr') : appPath('/fr/')}">fr</a></li>
-    </ul>
 `);
--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,5 @@
 (async () => {
-  const pathName =
-    typeof window.stripAppBasePrefix === 'function' ? window.stripAppBasePrefix(window.location.pathname) : window.location.pathname;
-  const isHomepage =
-    pathName === '/' || pathName === '/index.html' || pathName === '/fr' || pathName === '/fr/' || pathName === '/fr/index.html';
-  if (!isHomepage) {
-    return;
-  }
-
+  const pathName = window.location.pathname;
   const isFrench = pathName.startsWith('/fr/');
   const DISMISSED_POPUP_HASH_KEY = 'makerslab.dismissedPopupHash.v1';
 
@@ -81,9 +74,35 @@
     `;
   }
 
+  function noticeMarkup(content) {
+    return `
+      <div id="notice" class="red white-text center">
+        <span>${content}</span>
+      </div>
+    `;
+  }
+
+  function removeNotice() {
+    const existingNotice = document.getElementById('notice');
+    if (existingNotice) {
+      existingNotice.remove();
+    }
+  }
+
+  function showNotice(content) {
+    removeNotice();
+
+    const navbar = document.getElementById('navbar');
+    if (navbar) {
+      navbar.insertAdjacentHTML('afterend', noticeMarkup(content));
+      return;
+    }
+
+    document.body.insertAdjacentHTML('afterbegin', noticeMarkup(content));
+  }
+
   try {
-    const popupDataUrl = typeof window.appPath === 'function' ? window.appPath('/data/popup.json') : '/data/popup.json';
-    const response = await fetch(popupDataUrl);
+    const response = await fetch('/data/popup.json');
     if (!response.ok) {
       return;
     }
@@ -93,16 +112,21 @@
       return;
     }
 
-    const popupHash = popupIdentityHash(data.popup);
-    if (readDismissedPopupHash() === popupHash) {
-      return;
-    }
-
     const title = isFrench ? data.popup.titleFr : data.popup.titleEn;
     const content = isFrench ? data.popup.contentFr : data.popup.contentEn;
     if (!title || !content) {
       return;
     }
+
+    const popupHash = popupIdentityHash(data.popup);
+    const isDismissed = readDismissedPopupHash() === popupHash;
+    if (isDismissed) {
+      showNotice(content);
+      return;
+    }
+
+    // Hide any pre-existing static notice until the popup is dismissed.
+    removeNotice();
 
     const existingModal = document.getElementById('noticeModal');
     if (existingModal) {
@@ -119,6 +143,7 @@
     const instance = M.Modal.init(modalElement, {
       onCloseEnd: () => {
         writeDismissedPopupHash(popupHash);
+        showNotice(content);
       }
     });
     instance.open();
